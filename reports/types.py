d4352a7982370e453c33d56ed60ee5e272d13887
Krolm <tomaszpiotro@gmail.com>
2014-09-14 23:26:50 +0200

    all charts working. need only refactor

---
+++ reports/types.py
@@ -0,0 +1,33 @@
+from models import PrevOccurrences
+import datetime
+from datetime import timedelta
+import time
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)+1):
+        yield start_date + timedelta(n)
+
+
+class Occurrences():
+    def __init__(self, start_date, end_date, data):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.data = data
+
+    def __init__(self, occurrences):
+        self.data = self.occurrences_to_day_list(occurrences)
+
+    def occurrences_to_day_list(self, occurrences_by_day):
+        occurrences_by_day.sort(key=lambda tup: tup[0])
+        self.start_date = datetime.datetime.strptime(occurrences_by_day[0][0], '%Y-%m-%d').date()
+        self.end_date = datetime.datetime.strptime(occurrences_by_day[-1][0], '%Y-%m-%d').date()
+        occurrences = []
+        dates_only = [i[0] for i in occurrences_by_day]
+        for single_date in daterange(self.start_date, self.end_date):
+            date = time.strftime("%Y-%m-%d", single_date.timetuple())
+            if date in dates_only:
+                index = dates_only.index(date)
+                occurrences.append(occurrences_by_day[index][1])
+            else:
+                occurrences.append(0)
+        return occurrences
--- reports/views.py
+++ reports/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render_to_response
-from django.db.models import Count
 from reports.models import Report
 from reports.models import Reports_fitemsets
 from reports.models import FreqItemsets
 from reports.models import PrevOccurrences
 import itertools
-from graphos.renderers.highcharts import LineChart
-from graphos.sources.simple import SimpleDataSource
-
-
+from types import Occurrences
+import datetime
 
 def all_reports(request):
     rep = Report.objects.all().order_by('id').reverse()
     reports_list = []
     for r in rep:
-        reports_list.append((r, Reports_fitemsets.objects.filter(report_id=r.id).count()))
+        reports_list.append((r, Reports_fitemsets.objects.filter(report_id=r.id).count(),
+                             r.date+datetime.timedelta(hours=r.interval)))
+
     return render_to_response('reports.html', {'reports': reports_list})
 
 
@@ -26,16 +25,16 @@ def report(request, report_id=1):
         fitemsets.append(FreqItemsets.objects.get(id=f.fitemset_id))
     occurrences = PrevOccurrences.objects.filter(report=report_id)
     sets = []
+    occurrences_map = {}
     for f in fitemsets:
         sets.append((f, occurrences.filter(freq_itemset_id=f.id).count()))
+        grouped = itertools.groupby(occurrences.filter(freq_itemset_id=f.id), lambda record: record.date.strftime("%Y-%m-%d"))
+        occurrences_by_day = [(day, len(list(occurrences_this_day))) for day, occurrences_this_day in grouped]
+        occurrences_map.update({f.id: Occurrences(occurrences_by_day)})
     sets.sort(key=lambda tup: tup[0].remote_host)
     sets.sort(key=lambda tup: tup[1], reverse=True)
-    grouped = itertools.groupby(occurrences, lambda record: record.date.strftime("%Y-%m-%d"))
-    occurrences_by_day = [(day, len(list(occurrences_this_day))) for day, occurrences_this_day in grouped]
-    occurrences_by_day.insert(0, ('date', 'occurrences'))
-    print occurrences_by_day.__len__()
-    asd = occurrences_by_day[:50]
-    wykr = LineChart(SimpleDataSource(data=asd), html_id="bar_chart")
 
-    return render_to_response('report.html', {'report': reportt, 'reports_fitemsets': sets, 'report_id': report_id, 'chart': wykr})
+    end_date = reportt.date + datetime.timedelta(hours=reportt.interval)
+    return render_to_response('report.html', {'report': reportt, 'reports_fitemsets': sets, 'report_id': report_id,
+                                              'occurrences': occurrences_map, 'end_date': end_date})
 
