27c6df8b30bc9cfddbdc5f583db3846e6e185830
Krolm <tomaszpiotro@gmail.com>
2014-09-22 23:45:37 +0200

    added paging and mouseover

--- reports/types.py
+++ reports/types.py
@@ -3,6 +3,7 @@ import datetime
 from datetime import timedelta
 import time
 
+
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)+1):
         yield start_date + timedelta(n)
--- reports/views.py
+++ reports/views.py
@@ -6,15 +6,24 @@ from reports.models import PrevOccurrences
 import itertools
 from types import Occurrences
 import datetime
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def all_reports(request):
-    rep = Report.objects.all().order_by('id').reverse()
+    rep = Report.objects.all().order_by('date').reverse()
     reports_list = []
     for r in rep:
         reports_list.append((r, Reports_fitemsets.objects.filter(report_id=r.id).count(),
                              r.date+datetime.timedelta(hours=r.interval)))
-
-    return render_to_response('reports.html', {'reports': reports_list})
+    paginator = Paginator(reports_list, 100)
+    page = request.GET.get('page')
+    try:
+        reports = paginator.page(page)
+    except PageNotAnInteger:
+        reports = paginator.page(1)
+    except EmptyPage:
+        reports = paginator.page(paginator.num_pages)
+    return render_to_response('reports.html', {'reports': reports})
 
 
 def report(request, report_id=1):
@@ -27,7 +36,7 @@ def report(request, report_id=1):
     sets = []
     occurrences_map = {}
     for f in fitemsets:
-        sets.append((f, occurrences.filter(freq_itemset_id=f.id).count()))
+        sets.append((f, occurrences.filter(freq_itemset_id=f.id).count(), reports_fitemsets.get(fitemset_id=f.id)))
         grouped = itertools.groupby(occurrences.filter(freq_itemset_id=f.id), lambda record: record.date.strftime("%Y-%m-%d"))
         occurrences_by_day = [(day, len(list(occurrences_this_day))) for day, occurrences_this_day in grouped]
         occurrences_map.update({f.id: Occurrences(occurrences_by_day)})
@@ -35,6 +44,6 @@ def report(request, report_id=1):
     sets.sort(key=lambda tup: tup[1], reverse=True)
 
     end_date = reportt.date + datetime.timedelta(hours=reportt.interval)
-    return render_to_response('report.html', {'report': reportt, 'reports_fitemsets': sets, 'report_id': report_id,
+    return render_to_response('report.html', {'report': reportt, 'reports_fitemsets': sets,
                                               'occurrences': occurrences_map, 'end_date': end_date})
 
