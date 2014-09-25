f88ece3d87afb65a01b2ab14033febdda4c52cc6
Krolm <tomaszpiotro@gmail.com>
2014-09-25 15:23:47 +0200

    fixed bug with 0 occurrences

--- make_report.py
+++ make_report.py
@@ -24,7 +24,7 @@ def tuple_evaluate(t):
     return remove_index_from_tuple(tuple(lst), 0)
 
 
-interval = 10
+interval = 2
 print "running report script. Hours: " + interval.__str__()
 
 conn1 = psycopg2.connect(database="miner_copy", user="postgres", password="qwerty1asd", host="localhost")
--- reports/types.py
+++ reports/types.py
@@ -20,15 +20,16 @@ class Occurrences():
 
     def occurrences_to_day_list(self, occurrences_by_day):
         occurrences_by_day.sort(key=lambda tup: tup[0])
-        self.start_date = datetime.datetime.strptime(occurrences_by_day[0][0], '%Y-%m-%d').date()
-        self.end_date = datetime.datetime.strptime(occurrences_by_day[-1][0], '%Y-%m-%d').date()
         occurrences = []
-        dates_only = [i[0] for i in occurrences_by_day]
-        for single_date in daterange(self.start_date, self.end_date):
-            date = time.strftime("%Y-%m-%d", single_date.timetuple())
-            if date in dates_only:
-                index = dates_only.index(date)
-                occurrences.append(occurrences_by_day[index][1])
-            else:
-                occurrences.append(0)
+        if occurrences_by_day.__len__() > 0:
+            self.start_date = datetime.datetime.strptime(occurrences_by_day[0][0], '%Y-%m-%d').date()
+            self.end_date = datetime.datetime.strptime(occurrences_by_day[-1][0], '%Y-%m-%d').date()
+            dates_only = [i[0] for i in occurrences_by_day]
+            for single_date in daterange(self.start_date, self.end_date):
+                date = time.strftime("%Y-%m-%d", single_date.timetuple())
+                if date in dates_only:
+                    index = dates_only.index(date)
+                    occurrences.append(occurrences_by_day[index][1])
+                else:
+                    occurrences.append(0)
         return occurrences
